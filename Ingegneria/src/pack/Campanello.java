e67041163c70721919cd1e1dd94b388b58b7b886
david <david@DESKTOP-JG343PI>
2025-06-10 17:56:55 +0200

    ok

--- Ingegneria/src/pack/Campanello.java
+++ Ingegneria/src/pack/Campanello.java
@@ -1,5 +1,26 @@
 package pack;
 
 public class Campanello {
-
+private String nome;
+private String suono;
+public Campanello(String nome, String suono) {
+	super();
+	this.nome = nome;
+	this.suono = suono;
+}
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public String getSuono() {
+	return suono;
+}
+public void setSuono(String suono) {
+	this.suono = suono;
+}
+public String suona() {
+	return suono;
+}
 }
