404842c919cd24de540220d35fb77c73623dbcf9
david <david@DESKTOP-JG343PI>
2025-06-17 12:43:57 +0200

    risolto costruttore

--- Ingegneria/src/pack/CampanelloClassico.java
+++ Ingegneria/src/pack/CampanelloClassico.java
@@ -1,22 +1,10 @@
 package pack;
 
 public class CampanelloClassico implements Campanello {
-private String nome;
-//private String suono;
-public CampanelloClassico(String nome, String suono) {
-	super();
-	this.nome = nome;
-	//this.suono = suono;
+
+public CampanelloClassico() {
 }
-public String getNome() {
-	return nome;
-}
-public void setNome(String nome) {
-	this.nome = nome;
-}
-//public void setSuono(String suono) {
-//	this.suono = suono;
-//}
+
 @Override
 public void suona() {
 	System.out.println("Ding dong!");
