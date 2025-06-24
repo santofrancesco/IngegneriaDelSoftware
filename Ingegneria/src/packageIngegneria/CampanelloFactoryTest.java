ba7b174fcf42c25f983efceaebcfe07c2caa91ae
david <david@DESKTOP-JG343PI>
2025-06-24 12:22:17 +0200

    inizio di test

---
+++ Ingegneria/src/packageIngegneria/CampanelloFactoryTest.java
@@ -0,0 +1,14 @@
+package packageIngegneria;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CampanelloFactoryTest {
+
+	@Test
+	void test() {
+		assertTrue();
+	}
+
+}
