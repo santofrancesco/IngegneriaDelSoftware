aa387473a79f923ec144b6dd614ba5f37dabb3c6
david <david@DESKTOP-JG343PI>
2025-06-24 12:53:20 +0200

    test1 completato

--- Ingegneria/src/packageIngegneria/CampanelloFactoryTest.java
+++ Ingegneria/src/packageIngegneria/CampanelloFactoryTest.java
@@ -4,11 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import pack.Campanello;
+import pack.CampanelloClassico;
+
 class CampanelloFactoryTest {
 
 	@Test
 	void test() {
-		
+		Campanello campanello1 = CampanelloFactory.creaCampanello("classico");
+        
+        assertNotNull(campanello1);
+        assertTrue(campanello1 instanceof CampanelloClassico);
 	}
 
 }
